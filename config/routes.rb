d8773a14587d1e50462c856b85965bdb24b40d49
markhilkert <mark.hilkert@gmail.com>
2019-02-19 15:43:46 -0600

    added controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => 'users#index'
+    get "/users/:id" => 'users#show'
+    get '/users/new' => 'users#new'
+    post "/users" => 'users#create'
+    # post '/sessions' => 'sessions#create'
+    patch "/users/:id" => 'users#update'
+    delete "/users/:id" => 'users#destroy'
+  end
 end
