d8ba6c0ef8cd2aa82e55b52f74f708a814392c01
markhilkert <mark.hilkert@gmail.com>
2019-02-19 16:43:48 -0600

    added follower model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_222025) do
+ActiveRecord::Schema.define(version: 2019_02_19_224146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.boolean "approved", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
